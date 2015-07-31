63164f80e1661ed3dcc132b328315533222be986
Matthieu Brouillard <matthieu@brouillard.fr>
2015-07-31 09:59:36 +0200

    allow to send simple email notification to all active users of the system. minimal functionality to address restart/shutdown/upgrade communication as described in #852

--- src/main/scala/gitbucket/core/controller/AnnounceController.scala
+++ src/main/scala/gitbucket/core/controller/AnnounceController.scala
@@ -1,8 +1,12 @@
 package gitbucket.core.controller
 
 import gitbucket.core.admin.html
+import gitbucket.core.model.Account
+import gitbucket.core.service.{AccountService, SystemSettingsService}
+import gitbucket.core.servlet.Database
 import gitbucket.core.util.AdminAuthenticator
 import jp.sf.amateras.scalatra.forms._
+import org.apache.commons.mail.{DefaultAuthenticator, HtmlEmail}
 import org.slf4j.LoggerFactory
 
 /**
@@ -11,15 +15,16 @@ import org.slf4j.LoggerFactory
 class AnnounceController extends AnnounceControllerBase
   with AdminAuthenticator
 
-trait AnnounceControllerBase extends ControllerBase {
+trait AnnounceControllerBase extends ControllerBase with AccountService {
   self:  AdminAuthenticator =>
 
   private val logger = LoggerFactory.getLogger(classOf[AnnounceController])
 
-  case class AnnounceForm(content: String)
+  case class AnnounceForm(content: String, subject: String)
 
   private val announceForm = mapping(
-    "content" -> trim(label("Announce", text(required)))
+    "content" -> trim(label("Announce", text(required))),
+    "subject" -> trim(label("Subject", text(required)))
   )(AnnounceForm.apply)
 
   get("/admin/announce")(adminOnly {
@@ -31,6 +36,38 @@ trait AnnounceControllerBase extends ControllerBase {
 
     if (logger.isDebugEnabled) {
       logger.debug("sending announce: {}", form.content)
+    }
+
+    val systemSettings = new SystemSettingsService {}.loadSystemSettings
+    if (systemSettings.notification && systemSettings.smtp.nonEmpty) {
+      val email = new HtmlEmail
+      val smtp = systemSettings.smtp.get
+
+      email.setHostName(smtp.host)
+      email.setSmtpPort(smtp.port.get)
+      smtp.user.foreach { user =>
+        email.setAuthenticator(new DefaultAuthenticator(user, smtp.password.getOrElse("")))
+      }
+      smtp.ssl.foreach { ssl =>
+        email.setSSLOnConnect(ssl)
+      }
+      smtp.fromAddress
+        .map (_ -> smtp.fromName.orNull)
+        .orElse (Some("notifications@gitbucket.com" -> context.loginAccount.get.userName))
+        .foreach { case (address, name) =>
+        email.setFrom(address, name)
+      }
+      email.setCharset("UTF-8")
+      email.setSubject(form.subject)
+      email.setHtmlMsg(form.content)
+
+      logger.info("sending email: {}", form.content)
+      val database = Database()
+      database withSession { implicit session =>
+        getAllUsers(false).filter(account => !account.isGroupAccount && account.mailAddress.nonEmpty).foreach(account => email.addBcc(account.mailAddress))
+      }
+
+      email.send()
     }
 
     redirect("/admin/announce")
